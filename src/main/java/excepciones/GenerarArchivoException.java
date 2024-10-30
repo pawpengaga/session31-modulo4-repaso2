29426d013d1040fb16f259a88d463710f319408b
Erick Rivera <yomellamoerick.771@gmail.com>
2024-10-29 21:10:24 -0300

    excepcion lista

---
+++ src/main/java/excepciones/GenerarArchivoException.java
@@ -0,0 +1,15 @@
+package excepciones;
+
+public class GenerarArchivoException extends Exception {
+
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
+
+  public GenerarArchivoException(String message){
+    super(message);
+  }
+
+
+}
